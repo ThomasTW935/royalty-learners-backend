97faba755ca6cd20a8abbd91cc4ab027da668882
Daryl Thomas <thomastw935@gmail.com>
2021-11-29 08:27:28 +0800

    update
    Co-authored-by: Mymy1994 <Mymy1994@users.noreply.github.com>
    Co-authored-by: kevinkei1323 <kevinkei1323@users.noreply.github.com>
    Co-authored-by: Rupert Angelo Duazo <livingglory1@users.noreply.github.com>
    Co-authored-by: YellowCoder28 <YellowCoder28@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import express from "express";
+import cors from "cors"
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 const db = process.env.MONGODB_URI;
 
@@ -12,6 +14,12 @@ mongoose
   .connect(db, { newUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
+
+app.use('/login', (req,res)=>{
+  res.send({
+    token: 'test123'
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
