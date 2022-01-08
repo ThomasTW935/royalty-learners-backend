a9eafb8ade59c319b8b869f6ee48de4d51267965
Daryl Thomas <thomastw935@gmail.com>
2022-01-08 12:44:38 +0800

    update
    Co-authored-by: Rupert Angelo Duazo <livingglory1@users.noreply.github.com>
    Co-authored-by: kevinkei1323 <kevinkei1323@users.noreply.github.com>
    Co-authored-by: Mymy1994 <Mymy1994@users.noreply.github.com>

--- src/controllers/User/FetchUser.js
+++ src/controllers/User/FetchUser.js
@@ -1,27 +1,35 @@
-import User from '../../models/User.js'
+import User from "../../models/User.js";
 
-const getUser = async (req,res)=> {
-  try{
-    const user = await User.findById(req.user)
-    console.log(req.user)
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user);
+    console.log(req.user);
     res.json({
       id: user._id,
       first_name: user.first_name,
-      last_name: user.last_name
-    })
-  }catch(err){
-    res.status(500).json({error: err.message})
+      last_name: user.last_name,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-}
+};
 
-const getUsers = async (req,res)=> {
-  try{
-    const users = await User.find()
-    console.log(users)
-    res.json(users)
-  }catch(err){
-    res.status(500).json({error: err.message})
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    const formatUsers = users.map((user) => {
+      const newUser = {
+        _id: user._id,
+        username: user.username,
+        firstName: user.first_name,
+        lastName: user.last_name,
+      };
+      return newUser;
+    });
+    res.json(formatUsers);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-}
+};
 
-export {getUser, getUsers}
+export { getUser, getUsers };
